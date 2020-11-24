f9c9a1b17b2d3c33c32bfab12e55aec3bfd4d7de
Ramunas <zamrif@gmail.com>
2020-11-24 18:38:36 +0200

    form data adde to utils

---
+++ src/utils/FormData.js
@@ -0,0 +1,7 @@
+const formData = [
+  { id: 1, type: "text", name: "name", placeholder: "enter full name" },
+  { id: 2, type: "email", name: "email", placeholder: "enter your email" },
+  { id: 3, type: "textarea", name: "message", placeholder: "message" },
+];
+
+export default formData;
