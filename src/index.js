bdcf2bc8a04756cb2ebd817d855ae3f5908330a4
Ramunas <zamrif@gmail.com>
2020-11-24 18:41:24 +0200

    theme added to global index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Routes from "./Routes";
+import { ThemeProvider } from "styled-components";
+import theme from "./theme";
 import "normalize.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Routes />
+    <ThemeProvider theme={theme}>
+      <Routes />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
