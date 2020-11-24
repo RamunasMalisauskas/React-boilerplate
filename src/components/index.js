76dc2d298af6926352ea1265ea5f23c95b15a97b
Ramunas <zamrif@gmail.com>
2020-11-24 18:35:51 +0200

    component added to index export

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 export { default as Button } from "./Button/Button.jsx";
 export { default as Navigation } from "./Navigation/Navigation.jsx";
 export { default as Section } from "./Section/Section.jsx";
+export { default as FormTemplate } from "./FormTemplate/FormTemplate.jsx";
+export { default as Input } from "./Input/Input.jsx";
