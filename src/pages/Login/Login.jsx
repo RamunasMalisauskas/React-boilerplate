2684ff582738b192eadbb75d2a2447c8ccf1ecaf
Ramunas <zamrif@gmail.com>
2020-11-30 22:24:42 +0200

    Notification and history push added

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,11 @@
 import React, { useContext } from "react";
+import { useHistory } from "react-router-dom";
 import { AuthContext } from "../../contexts/AuthContext";
-import { FormTemplate, Section } from "../../components/";
+import { FormTemplate, Section, Notification } from "../../components/";
 import loginFormData from "../../utils/LoginFormData";
+import { useState } from "react";
 
-function Login(fieldValues, auth) {
+function Login(fieldValues, auth, history, setError) {
   fetch("http://localhost:8081/login", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
@@ -13,22 +15,40 @@ function Login(fieldValues, auth) {
     }),
   })
     .then((res) => res.json())
-    .then(
-      (data) => auth.setToken("Bearer " + data.token) + console.log(data.token)
-    )
-    .catch((err) => console.log("error: " + err));
+    .then((data) => {
+      if (data.token) {
+        auth.setToken("Bearer " + data.token);
+        history.push("/");
+      } else {
+        setError({ status: true, msg: data.msg || "input error" });
+      }
+    })
+    .catch((err) => {
+      setError({ status: true, msg: "server error" });
+      console.log(err);
+    });
 }
 
 function LoginPage() {
+  const history = useHistory();
   const auth = useContext(AuthContext);
+  const [error, setError] = useState({ status: false, msg: "error" });
 
   return (
     <>
       <Section>
+        {error.status && (
+          <Notification
+            notificationMessage={error.msg}
+            handleClick={() => setError({ status: false })}
+            color="error"
+          />
+        )}
         <h1>login</h1>
-
         <FormTemplate
-          callback={(fieldValues) => Login(fieldValues, auth)}
+          callback={(fieldValues) =>
+            Login(fieldValues, auth, history, setError)
+          }
           fields={loginFormData}
           buttonText="Login"
           buttonType="submit"
